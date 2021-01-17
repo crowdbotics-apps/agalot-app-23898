daa2da629ac62b205a5b20b17a343f537e4c625f
Crowdbotics <team@crowdbotics.com>
2021-01-17 17:10:27 +0000

    Update models

--- backend/agalot_app_23898/wsgi.py
+++ backend/agalot_app_23898/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agalot_app_23898.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agalot_app_23898.settings")
 
 application = get_wsgi_application()
